2c3ca71185a1c4589db41a17b1d3abd411cd1403
ryukgod26 <ekravi26@gmail.com>
2025-08-14 18:32:16 +0000

    Learned Basic Concepts

---
+++ script.js
@@ -0,0 +1,18 @@
+
+var box = documet.querySelector("a-box");
+
+box.addEventListener('mouseenter',()=>{
+
+box.setAttribute('scale',{x:2,y:2,z:2});
+
+});
+
+box.addEventListener('mouseleave',()=>{
+
+box.setAttribute('scale',{x:2,y:2,z:2});
+
+});
+
+
+
+
